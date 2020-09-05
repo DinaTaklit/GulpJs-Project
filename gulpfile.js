ecc0584a2e8046ae689cc8aed345cc6ed19e6fda
DinaTaklit <ez_taklit@esi.dz>
2020-09-06 00:29:09 +0100

    feat: Live Reload Browser

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ const concat = require('gulp-concat');
 const prefix = require('gulp-autoprefixer');
 const sass = require('gulp-sass');
 const pug = require('gulp-pug');
+const livereload = require('gulp-livereload');
 
 // html task 
 gulp.task('html-task', async function(){
     return gulp.src('project/index.pug')
             .pipe(pug({pretty: true}))
             .pipe(gulp.dest('dist'))
+            .pipe(livereload());
 });
 
 // css task 
@@ -20,11 +22,13 @@ gulp.task('css-task',async function(){
             //later if we have other css libraries that we want to add
             .pipe(concat('main.css')) 
             .pipe(gulp.dest('dist/public/css'))
+            .pipe(livereload());
 });
 
 // watch task
 gulp.task('watch-task', async function(){
     require('./server.js');
+    livereload.listen();
     gulp.watch(['project/index.pug','project/pug/*'], gulp.series('html-task'));
     gulp.watch(['project/public/sass/*','project/public/sass/components/*'], gulp.series('css-task'));
 });
