623fd0f999a53f5d7cee0f9e880e74614bd52056
DinaTaklit <ez_taklit@esi.dz>
2020-09-05 18:26:12 +0100

    feat: AutoPrefixer For CSS3 Properties

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp'); // to import gulp from gulp modules
 const concat = require('gulp-concat');
+const prefix = require('gulp-autoprefixer');
 // html task 
 gulp.task('html-task', async function(){
     return gulp.src(['project/index.html','project/about.html'])
@@ -10,6 +11,7 @@ gulp.task('html-task', async function(){
 
 gulp.task('css-task',async function(){
     return gulp.src('project/public/css/*')
+            .pipe(prefix('last 2 versions'))
             .pipe(concat('main.css'))
             .pipe(gulp.dest('dist/public/css'))
 });
