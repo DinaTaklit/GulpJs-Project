d5b67fda50d0aa0356ce964ad26edc53dba74e28
DinaTaklit <ez_taklit@esi.dz>
2020-09-05 15:03:52 +0100

    fix: tasks did not complete: first-task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ const gulp = require('gulp'); // to import gulp from gulp modules
 
 // My first task 
 
-gulp.task('first-task', function(){
+gulp.task('first-task', async function(){
     console.log("Yeessss My fist task :D");
 });
