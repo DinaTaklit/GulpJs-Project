8a8001e163daaeb70c926337260da063e07307a3
DinaTaklit <ez_taklit@esi.dz>
2020-09-05 17:52:32 +0100

    feat: Concatenate CSS and JS Files In One File

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp'); // to import gulp from gulp modules
-
-// My first task 
-
-gulp.task('first-task', async function(){
+const concat = require('gulp-concat');
+// html task 
+gulp.task('html-task', async function(){
     return gulp.src(['project/index.html','project/about.html'])
             .pipe(gulp.dest('dist'))
+});
+
+// css task 
+
+gulp.task('css-task',async function(){
+    return gulp.src('project/public/css/*')
+            .pipe(concat('main.css'))
+            .pipe(gulp.dest('dist/public/css'))
 });
